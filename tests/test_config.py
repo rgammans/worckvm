16d74352b9ad1164a4d48f92c325913f3abeed8b
Roger Gammans <rgammans@gammascience.co.uk>
2023-11-12 15:22:17 +0000

    Add configuration importer.
    Currently not quite possible to seperate internal matrix topology config,
    from it's connectiosn tothe outside.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,10 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from unittest.mock import Mock
+from textwrap import dedent
 
 from worchestic.matrix import Matrix
+from worchestic.signals import Source
+from worchestic.group import SourceGroup, MatrixGroup
 
 from worckvm import config
 
@@ -41,9 +44,216 @@ class ConfigTest(TestCase):
   nr_inputs: 6
   nr_outputs: 2
 - !MatrixOutput
-  name: "video"
+  matrix_name: "video"
   output_idx: 1
 """)
         matrix = system[0]
         self.assertTrue(isinstance(matrix, Matrix))
         self.assertIs(matrix.outputs[1], system[1])
+
+    def test_inviald_matrix_output_raises_useful_error(self):
+        with self.assertRaises(config.MissingConfigKey):
+            _ = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 6
+  nr_outputs: 2
+- !MatrixOutput
+  matrix_name: "video"
+  ouput_idx: 1
+""")
+
+    @skip("not meeaniful whitebox")
+    def test_matrix_input_reference_current_attribte_matches_the_matrix(self):
+        system = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 6
+  nr_outputs: 2
+- !MatrixInput
+  matrix_name: "video"
+  input_idx: 1
+""")
+        matrix = system[0]
+        self.assertIs(matrix.inputs[1], system[1]._current)
+
+    def test_matrix_input_ref_sets_attribute_set_the_value_on_matrix(self):
+        system = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 6
+  nr_outputs: 2
+- !MatrixInput
+  matrix_name: "video"
+  input_idx: 1
+""")
+        matrix = system[0]
+        new_source = Source("test")
+        system[1].set_to(new_source)
+        self.assertIs(matrix.inputs[1], new_source)
+
+    def test_multpleconnections_to_the_same_input_raise(self):
+        system = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 6
+  nr_outputs: 2
+- !MatrixInput
+  matrix_name: "video"
+  input_idx: 1
+""")
+        new_source1 = Source("test")
+        new_source2 = Source("test")
+        system[1].set_to(new_source1)
+        with self.assertRaises(config.DuplicateConnection):
+            system[1].set_to(new_source2)
+
+    def test_matrix_group_raises_sensible_error_with_missing_attributes(self):
+        with self.assertRaises(config.MissingConfigKey):
+            config.loads("""
+- !MatrixGroup
+    misspelling: "video"
+    other: "text"
+""")
+
+    def test_sourcesets_can_be_defined(self):
+        system = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 4
+  nr_outputs: 1
+
+- !Matrix
+  name: "hid"
+  nr_inputs: 4
+  nr_outputs: 1
+
+- !MatrixOutput &video
+  matrix_name: "video"
+  output_idx: 0
+
+- !MatrixOutput &hid
+  matrix_name: "hid"
+  output_idx:  0
+
+- !SourceSet
+  name: "Gaming PC"
+  sources:
+    - type: "video"
+      preferred_output: *video
+      connected_to: !MatrixInput
+          matrix_name: "video"
+          input_idx: 1
+
+    - type: "hid"
+      preferred_output: *hid
+      connected_to: !MatrixInput
+          matrix_name: "hid"
+          input_idx: 1
+
+- !SourceSet
+  name: "Mac"
+  sources:
+      - type: "video"
+        preferred_output: *video
+        connected_to: !MatrixInput
+            matrix_name: "video"
+            input_idx: 2
+
+      - type: "hid"
+        preferred_output: *hid
+        connected_to: !MatrixInput
+            matrix_name: "hid"
+            input_idx: 2
+
+
+  """)
+        # Check name
+        self.assertEqual(system[4].name, "Gaming PC")
+        # Check correct numnber of source
+        self.assertEqual(len(system[4].sources), 2)
+        # Check defaul sorue name.
+        self.assertEqual(system[4].sources['video'].name, "Gaming PC (video)")
+        # . and output
+        self.assertIs(system[4].sources['video'].preferred_out, system[2])
+        # and check connection
+        self.assertIs(system[4].sources['video'], system[0].inputs[1])
+
+    def test_sourcesets_can_opnly_have_single_soure_of_a_single_type(self):
+        cfg = dedent("""
+            - !Matrix
+              name: "video"
+              nr_inputs: 4
+              nr_outputs: 1
+
+            - !SourceSet
+              name: "Gaming PC"
+              sources:
+                - type: "video"
+                - type: "video"
+              """)
+        with self.assertRaises(config.DuplicateSourceType):
+            config.loads(cfg)
+
+    def test_sourcesets_combine_into_a_sourcegroup(self):
+        system = config.loads("""
+- !Matrix
+  name: "video"
+  nr_inputs: 4
+  nr_outputs: 1
+
+- !Matrix
+  name: "hid"
+  nr_inputs: 4
+  nr_outputs: 1
+
+- !MatrixOutput &video
+  matrix_name: "video"
+  output_idx: 0
+
+- !MatrixOutput &hid
+  matrix_name: "hid"
+  output_idx:  0
+
+- !MatrixGroup
+  matricies: [ "video", "hid" ]
+  sources:
+  - !SourceSet
+    name: "Gaming PC"
+    sources:
+      - type: "video"
+        name: Gaming PC HDMI
+        preferred_output: *video
+        connected_to: !MatrixInput
+            matrix_name: "video"
+            input_idx: 1
+
+      - type: "hid"
+        preferred_output: *hid
+        connected_to: !MatrixInput
+            matrix_name: "hid"
+            input_idx: 1
+
+  - !SourceSet
+    name: "Mac"
+    sources:
+        - type: "video"
+          preferred_output: *video
+          connected_to: !MatrixInput
+              matrix_name: "video"
+              input_idx: 2
+
+        - type: "hid"
+          preferred_output: *hid
+          connected_to: !MatrixInput
+              matrix_name: "hid"
+              input_idx: 2
+
+
+  """)
+        self.assertEqual(set(system[4].matricies.keys()), {"video", 'hid'})
+        self.assertEqual(system[4].matricies['video'], system[0])
+        self.assertEqual(set(system[4].signals.groups.keys()),
+                         {"video", "hid"})
+        self.assertEqual(system[4].signals.groups['video'][0].name,
+                         "Gaming PC HDMI")
--- worckvm/config.py
+++ worckvm/config.py
@@ -1,10 +1,26 @@
 import io
 import yaml
+from dataclasses import dataclass
+from typing import List
 
 from worchestic.matrix import Matrix
+from worchestic.group import SourceGroup, MatrixGroup
+from worchestic.signals import Source
 
 _matrixes = {}
 
+
+class DuplicateConnection(ValueError):
+    """Multiple device connected to the same input"""
+
+class MissingConfigKey(ValueError):
+    """An expected value was missing from a config object"""
+
+class NoSuchMatrix(KeyError):
+    """Cant find a  matcging amtrix"""
+
+class DuplicateSourceType(ValueError):
+    """A sourceset can only contain a single source of each type"""
 
 def set_matrix(name, matrix):
     global _matrixes
@@ -12,7 +28,10 @@ def set_matrix(name, matrix):
 
 
 def get_matrix(name):
-    return _matrixes[name]
+    try:
+        return _matrixes[name]
+    except KeyError as e:
+        raise NoSuchMatrix(*e.args)
 
 
 def yaml_tag(name: str):
@@ -41,8 +60,70 @@ def build_matrix(loader, node):
 @yaml_tag("MatrixOutput")
 def get_matrix_output(loader, node):
     data = loader.construct_mapping(node)
-    m = get_matrix(data['name'])
-    return m.outputs[data['output_idx']]
+    try:
+        m = get_matrix(data['matrix_name'])
+        return m.outputs[data['output_idx']]
+    except KeyError as e:
+        raise MissingConfigKey(*e.args)
+
+@yaml_tag("MatrixInput")
+class MatrixInputProxy:
+    def __init__(self, loader, node):
+        data = loader.construct_mapping(node)
+        self.matrix = get_matrix(data['matrix_name'])
+        self.idx = data['input_idx']
+
+    def set_to(self, src):
+        if self.matrix.inputs[self.idx] is not None:
+            raise DuplicateConnection()
+        self.matrix.replug_input(self.idx, src)
+
+
+@yaml_tag("MatrixGroup")
+def matrixgroup(loader, node):
+    data = loader.construct_mapping(node, deep=True)
+    mats = {}
+
+    # Process matricies
+    try:
+        for matrix_name in data['matricies']:
+            mats[matrix_name] = get_matrix(matrix_name)
+    except KeyError as e:
+        raise MissingConfigKey(*e.args)
+
+    # Process source sets.
+    groups = {}
+    grp_len = len(data['sources'])
+    for idx, srcset in enumerate(data['sources']):
+        for src_type, src in srcset.sources.items():
+            # Assign each source to a SourceGroup type, using the sourceset idx
+            # as the soruce idx in that group
+            groups.setdefault(src_type, [None] * grp_len)[idx] = src
+
+    return MatrixGroup(SourceGroup(**groups), **mats)
+
+
+@yaml_tag("SourceSet")
+class SourceSet:
+    ""
+    def __init__(self, loader, node):
+        data = loader.construct_mapping(node, deep=True)
+        self.name = data['name']
+        self.sources = {}
+        for src in data['sources']:
+            src_type = src['type']
+            if src_type in self.sources:
+                raise DuplicateSourceType(src_type)
+            self.sources[src_type] = Source(
+                name=src.get('name', f"{self.name} ({src_type})"),
+                preferred_out=src.get('preferred_output'),
+            )
+            if connection := src.get('connected_to'):
+                # Connection is expected to in a MatrixInputProxy
+                connection.set_to(self.sources[src_type])
+
+
+
 
 
 def loads(str_data: str):
